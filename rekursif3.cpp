a892e4922358fa34cf8c94e5f96b5914a8466d16
d0nqiote <hayloveyou4ever@gmail.com>
2025-09-30 15:53:32 +0700

    rekursif implementasi suatu rumus fungsi

---
+++ rekursif3.cpp
@@ -0,0 +1,20 @@
+/*
+Nama program    :Pengimplementasian rumus f(n) = 2f(n-1) + 4, untuk n > 0
+NPM             :140810250049
+Tanggal dibuat  :30 September 2025
+Waktu awal      :15:42
+Waktu akhir     :15:53
+*/
+
+#include <iostream>
+using namespace std;
+
+int fungsiN(int n){
+    if(n == 0) return 3;
+    return 2 * fungsiN(n - 1) + 4;
+}
+
+int main(){
+    int n; cin >> n;
+    cout << fungsiN(n);
+}
