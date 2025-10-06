a272b2a3051c076a65d83b7c83c8a4af56240ab9
d0nqiote <hayloveyou4ever@gmail.com>
2025-10-06 08:05:02 +0700

    nambahin komen?

--- loop.cpp
+++ loop.cpp
@@ -27,13 +27,13 @@ void faktorial(int n){ //awal pengerjaan :07:21, akhir pengerjaan : 07:31
     cout << hasil;
 }
 
-void bintangKaki(int n){ //awal pengerjaan 07:34, akhir pengerjaan :
+void bintangKaki(int n){ //awal pengerjaan 07:34, akhir pengerjaan : 08:04
     cin >> n;
     for(int i = 1; i <= n; i++){ //baris
-        for(int h = 1; h <= n; h++){ 
-            for(int j = 1; j <= n - i; j++) cout << " ";
-            for(int j = 1; j <= 2 * i - 1; j++) cout << "*";
-            for(int k = 1; k <= n - i; k++) cout << " ";
+        for(int h = 1; h <= n; h++){ //banyak segitiga
+            for(int j = 1; j <= n - i; j++) cout << " "; //spasi kiri
+            for(int j = 1; j <= 2 * i - 1; j++) cout << "*"; //bintang
+            for(int k = 1; k <= n - i; k++) cout << " "; //spasi kanan
             cout << " ";
         }
         cout << endl;
